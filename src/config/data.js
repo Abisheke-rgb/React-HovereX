ced672d3339b8908e764548033de9ae94c26f4ac
Abisheke-rgb <61817853+Abisheke-rgb@users.noreply.github.com>
2022-04-08 12:55:25 +0530

    initiate

---
+++ src/config/data.js
@@ -0,0 +1,18 @@
+export const chartDays = [
+  {
+    label: "24 Hours",
+    value: 1,
+  },
+  {
+    label: "1 month",
+    value: 30,
+  },
+  {
+    label: "1 year",
+    value: 365,
+  },
+  {
+    label: "5 Year",
+    value: 1826,
+  },
+];
